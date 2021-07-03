fe3c403bae08461df2ee3a9ea23d67d2f981eaf8
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-07-03 14:41:52 +0100

    added GitHub URL

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      E-commerce Project
-    </div>
+      <h1>E-commerce Project</h1>
+      <footer> This project was coded by <a href="https://www.blakbdavies.com" target="_blank" rel="noreferrer">Blake Davies</a> and is 
+      <a href="https://github.com/blakebdavies/react-ecomm-project" target="_blank" rel="noreferrer"> open-sourced on Github</a>
+      </footer>
+      </div>
   );
 }
 
