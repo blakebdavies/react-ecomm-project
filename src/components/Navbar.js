dbf3d19753bfa492201c1148a06b10ebe09a8488
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-07-14 14:29:46 +0100

    created navbar component and updated initial logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
+import logo from "../logo.png";
+
 
 export default class Navbar extends Component {
     render(){
         return(
             <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <div class="container">
+               {/*
+               <div>Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/" 
+               title="Flaticon">www.flaticon.com</a></div>}*/}
                <Link to="/">
-                   <div className="navbar-brand">
-                   <i className="fas fa-home"></i>
-                   </div>
+                   <img src={logo} alt="logo"
+                   className="navbar-brand" width="40" height="50"/>
                </Link>
                <ul className="navbar-nav align-items-center">
                    <li className="nav-item ml-5">
@@ -23,6 +28,7 @@ export default class Navbar extends Component {
                        My Cart
                    </button>
                </Link>
+               </div>
             </nav>
         )
     }
