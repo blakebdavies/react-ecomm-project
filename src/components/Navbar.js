5e7a711bff9a0a841dde427bc62d9e85f76ffdfb
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-07-14 15:07:49 +0100

    Fleshed out navbar structure, installed styled components and updated styling of cart button and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
 import logo from "../logo.png";
-
+import styled from 'styled-components';
+import { ButtonContainer } from './Button'
 
 export default class Navbar extends Component {
     render(){
         return(
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 <div class="container">
                {/*
-               <div>Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/" 
-               title="Flaticon">www.flaticon.com</a></div>}*/}
+               Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/" 
+               title="Flaticon">www.flaticon.com</a>}*/}
                <Link to="/">
                    <img src={logo} alt="logo"
                    className="navbar-brand" width="40" height="50"/>
@@ -23,13 +24,23 @@ export default class Navbar extends Component {
                    </li>
                </ul>
                <Link to="/cart" className="ml-auto">
-                   <button>
+                   <ButtonContainer>
+                       <span className="mr-2">
                        <i className="fas fa-cart-plus" />
                        My Cart
-                   </button>
+                       </span>
+                   </ButtonContainer>
                </Link>
                </div>
-            </nav>
+            </NavWrapper>
         )
     }
 }
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+    color: var(--mainWhite)!important;
+    font-size:1.3rem;
+    text-transform: campitalize;
+}
+`;
