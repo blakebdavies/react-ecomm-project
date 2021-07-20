8abe41dbe1611a412b2829a013731b28fef7d169
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-07-20 22:11:59 +0100

    fleshed out cart display and incorporated increment, decrement, and remove functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,13 +2,34 @@
 //doing this allows you to avoid creating multiple index.js files and proper relative paths down to the file
 import React, { Component } from 'react';
 import Title from '../Title';
-
+import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
+import {ProductConsumer} from '../../context';
+import CartList from './CartList';
 
 export class Cart extends Component {
     render() {
         return (
             <section>
-                <Title name="your" title="cart" />
+                <ProductConsumer>
+                    {value => {
+                        const {cart} =value;
+                        if(cart.length > 0){
+                            return(
+                        <React.Fragment>
+                            <Title name="your" title="cart" />
+                            <CartColumns />
+                            <CartList  value={value} />
+                        </React.Fragment>
+                            );
+                        } else{
+                            return (
+                        <EmptyCart />
+                            )
+                        }
+                    }}
+                </ProductConsumer>
+            
             </section>
 
         )
