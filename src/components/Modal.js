d5c32249b53f153a698a1b7568061df5829e5f37
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-07-26 17:28:45 +0100

    improved css, changed logo and favicon, improved navigation

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,70 +1,73 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import {ProductConsumer} from "../context";
-import {ButtonContainer} from './Button'
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { ProductConsumer } from "../context";
+import { ButtonContainer } from "./Button";
+import { Link } from "react-router-dom";
 
 export default class Modal extends Component {
-    render () {
-        return (
-           <ProductConsumer>
-               {(value)=>{
-                   const {modalOpen, closeModal} = value;
-                   const {img, title, price} = value.modalProduct;
+  render() {
+    return (
+      <ProductConsumer>
+        {(value) => {
+          const { modalOpen, closeModal } = value;
+          const { img, title, price } = value.modalProduct;
 
-                   if(!modalOpen){
-                       return null;
-                   } else{
-                       return(
-                    <ModalContainer>
-                        <div className = "container">
-                            <div className= "row">
-                                <div id="modal" className="col-8 mx-auto col-md-6 col lg-4
-                                 text-center text capitalize p-5">\
-                                 <h5>item added to the cart</h5>
-                                 <img 
-                                 src={img} 
-                                 className="img-fluid" 
-                                 alt="product"/>
-                                 <h5>{title}</h5>
-                                 <h5 className="text-muted">price: $ {price}</h5>
-                                 <Link to="/">
-                                     <ButtonContainer
-                                     onClick={()=>closeModal()}>
-                                    Store
-                                     </ButtonContainer>
-                                 </Link>
-                                 <Link to="/cart">
-                                     <ButtonContainer cart
-                                     onClick={()=>closeModal()}>
-                                    Go to Cart
-                                     </ButtonContainer>
-                                 </Link>
-                                 
-                                 </div>
-                            </div>
-                        </div>
-
-                    </ModalContainer>
-                       )}
-               }}
-           </ProductConsumer>
-        );
-    } 
+          if (!modalOpen) {
+            return null;
+          } else {
+            return (
+              <ModalContainer>
+                <div className="container">
+                  <div className="row">
+                    <div
+                      id="modal"
+                      className="col-8 mx-auto col-md-6 col lg-4
+                                 text-center text capitalize p-5"
+                    >
+                      <h5>item added to the cart</h5>
+                      <img src={img} className="img-fluid" alt="product" />
+                      <h5>{title}</h5>
+                      <h5 className="text-muted">price: $ {price}</h5>
+                      <Link to="/">
+                        <button
+                          className="btn btn-danger m-2"
+                          onClick={() => closeModal()}
+                        >
+                          Store
+                        </button>
+                      </Link>
+                      <Link to="/cart">
+                        <button
+                          className="btn btn-warning m-2"
+                          cart
+                          onClick={() => closeModal()}
+                        >
+                          Go to Cart
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </ModalContainer>
+            );
+          }
+        }}
+      </ProductConsumer>
+    );
+  }
 }
 
 const ModalContainer = styled.div`
-position: fixed;
-top: 0;
-left: 0;
-right: 0;
-bottom: 0;
-background: rgba(0,0,0,0.3);
-display: flex;
-align-item: center;
-justify-content: center;
-#modal{
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-item: center;
+  justify-content: center;
+  #modal {
     background: var(--mainWhite);
-}
-
+  }
 `;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,106 @@
-import React, { Component } from 'react';
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import logo from "../logo.png";
-import styled from 'styled-components';
-import { ButtonContainer } from './Button'
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Navbar extends Component {
-    render(){
-        return(
-            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-                <div class="container">
-               {/*
+  render() {
+    return (
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <div class="container">
+          {/*
                Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/" 
                title="Flaticon">www.flaticon.com</a>}*/}
-               <Link to="/">
-                   <img src={logo} alt="logo"
-                   className="navbar-brand" width="40" height="50"/>
-               </Link>
-               <ul className="navbar-nav align-items-center">
-                   <li className="nav-item ml-5">
-                       <Link to="/" className="nav-link">
-                           Products
-                       </Link>
-                   </li>
-               </ul>
-               <Link to="/cart" className="ml-auto">
-                   <ButtonContainer>
-                       <span className="mr-2">
-                       <i className="fas fa-cart-plus" />
-                       My Cart
-                       </span>
-                   </ButtonContainer>
-               </Link>
-               </div>
-            </NavWrapper>
-        )
-    }
+          <Link to="/">
+            <img
+              src={logo}
+              alt="logo"
+              className="navbar-brand"
+              width="100"
+              height="90"
+            />
+          </Link>
+          <ul className="navbar-nav align-items-center">
+            <li className="nav-item p-2 ml-5">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item p-2 ml-5">
+              <Link to="/" className="nav-link">
+                Shop
+              </Link>
+            </li>
+            <li className="nav-item p-2 ml-5">
+              <Link to="/" className="nav-link">
+                About
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav align-items-center">
+            <li className="nav-item">
+              <Link to="/" className="ml-auto">
+                <ButtonContainer>
+                  <span className="mr-2">
+                    <i className="fas fa-search"></i>
+                  </span>
+                </ButtonContainer>
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <Link
+                to="/"
+                className="nav-link ml-auto"
+                role="button"
+                id="navbarDropdown"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <ButtonContainer>
+                  <span className="mr-2">
+                    <i className="fas fa-user"></i>
+                  </span>
+                </ButtonContainer>
+              </Link>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <Link class="dropdown-item" href="#">
+                  Login to My Vodafone
+                </Link>
+                <Link className="dropdown-item" href="#">
+                  Upgrade
+                </Link>
+                <div className="dropdown-divider"></div>
+                <Link className="dropdown-item" href="#">
+                  Existing Customer Deals
+                </Link>
+              </div>
+            </li>
+            <li className="nav-item">
+              <Link to="/cart" className="ml-auto">
+                <ButtonContainer>
+                  <span className="mr-2">
+                    <i className="fas fa-shopping-cart" />
+                  </span>
+                </ButtonContainer>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </NavWrapper>
+    );
+  }
 }
 const NavWrapper = styled.nav`
-background: var(--mainBlue);
-.nav-link{
-    color: var(--mainWhite)!important;
-    font-size:1.3rem;
-    text-transform: campitalize;
-}
+  background: var(--primary-color);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+    transition: all 150ms ease-in-out;
+  }
+  .nav-link:hover {
+    color: var(--secondary-color) !important;
+  }
 `;
