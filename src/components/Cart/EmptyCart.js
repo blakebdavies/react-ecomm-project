0ffa278c7335acf356aab2f75212848cb881686f
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-07-26 19:29:38 +0100

    added button to empty cart component

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function EmptyCart() {
-    return (
-        <div className="container nt-5">
-            <div className="row">
-                <div className="col-10 mx-auto text-center text-title">
-                    <h1>your cart is currently empty</h1>
-                </div>
-            </div>
+  return (
+    <div className="container mt-3 mb-5">
+      <div className="row">
+        <div className="col-12 mx-auto text-center text-title m-5">
+          <h1>your cart is currently empty</h1>
         </div>
-    )
+        <div className="col-12 mx-auto text-center text-title mb-5">
+          <Link to="/">
+            <button
+              type="button"
+              className="btn btn-outline-danger text-capitalize"
+            >
+              Go Back to Shop
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
